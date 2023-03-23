919587ae9fe72047c027692d4cc3ffa982f5e453
Sara El Mir <ingelmirsara@gmail.com>
2023-03-22 18:02:48 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,13 +2,10 @@
  * File: 101-natural.c
  * Auth: Sara El Mir 
  */
-
 #include <stdio.h>
-
 /**
  * main - Lists all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
- *
  * Return: Always 0.
  */
 int main(void)
@@ -20,8 +17,6 @@ int main(void)
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
-
 	printf("%d\n", sum);
-
 	return (0);
 }
