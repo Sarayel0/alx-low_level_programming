12a87296b0d7212e8e90a17b37d0481ad3386c53
Sara El Mir <ingelmirsara@gmail.com>
2023-03-23 17:49:41 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,20 +9,18 @@
  */
 
 int largest_number(int a, int b, int c)
-{int largest;
+{
+    int largest = a;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+    if (b > largest)
+    {
+        largest = b;
+    }
 
-return (largest);
+    if (c > largest)
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
